893a638e7c7afd5d85b51857ae18fe32f29e6098
Nikita Balikhin <ramiro.cruz@mail.ru>
2023-03-12 16:20:16 +0300

    Реализация блога на Astro

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Hacknote.js
 
-![Hacknote.js](./images/logo.png)
+![Hacknote.js](./public/images/logo.png)
 
 Репозиторий со статьями для Telegram-канала [Hacknote.js](https://t.me/hacknote_js).
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import customTheme from "./color-scheme.json";
 
 // https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://newesters.github.io",
+  base: "/hacknote-js",
   integrations: [mdx(), sitemap(), react()],
   markdown: {
     shikiConfig: {
-      theme: "monokai",
+      theme: customTheme,
     },
   },
 });
