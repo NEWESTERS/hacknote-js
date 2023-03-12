5d293783a9b589effa6f60a45c02f9af0a33dfe7
Nikita Balikhin <ramiro.cruz@mail.ru>
2023-03-12 16:35:12 +0300

    SITE_DESCRIPTION

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Hacknote.js";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION =
+  "Блог со статьями для Telegram-канала Hacknote.js.";
